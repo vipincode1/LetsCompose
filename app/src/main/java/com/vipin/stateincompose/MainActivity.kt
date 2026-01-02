f78dc489a3feb3bb3fc3ae319720abcf66d011e0
vipinvinayak <vipin.maurya19@gmail.com>
2026-01-03 00:51:52 +0530

    managing state using remember and mutableStateOf(0).

--- app/src/main/java/com/vipin/stateincompose/MainActivity.kt
+++ app/src/main/java/com/vipin/stateincompose/MainActivity.kt
@@ -1,16 +1,24 @@
 package com.vipin.stateincompose
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.vipin.stateincompose.ui.theme.StateInComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +27,43 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             StateInComposeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                )
+                {
+
+                    LearnState()
+
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    StateInComposeTheme {
-        Greeting("Android")
+fun LearnState() {
+/*    var age = 0*/
+
+    var age by remember {
+
+        mutableStateOf(0)
     }
+
+
+    Column(modifier = Modifier.fillMaxSize(),
+        Arrangement.Center,Alignment.CenterHorizontally)
+    {
+
+        Button(onClick = {
+            age++
+
+            Log.v("TAG", "age " + age)
+        }) {
+
+            Text(text = "I am $age years")
+        }
+    }
+
 }
